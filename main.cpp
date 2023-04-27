ac146d4fe5c4970d367189cff30b093945584054
liquisss <119260541+liquisss@users.noreply.github.com>
2023-04-27 22:12:17 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+class Detail {
+protected:
+	Detail() {}
+  
+public:
+	virtual ~Detail() {}
+	template <typename T>friend void Add(std::vector<Detail*>& v);
+};
+
+class Assembly :public Detail {
+protected:
+	Assembly() {}
+  
+public:
+	virtual ~Assembly() {}
+	template <typename T>friend void Add(std::vector<Detail*>& v);
+};
+
+template<typename T>
+void Add(std::vector<Detail*>& v)
+{
+	v.push_back(new T());
+}
+
+int main()
+{
+	std::vector<Detail*> v;
+	Add<Detail>(v);
+	Add<Assembly>(v);
+
+	for (auto obj : v) {
+		delete obj;
+	}
+	
